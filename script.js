79f4af622a8378be9b6325f34411edcdafdefcc3
Elis Honorina <elishonorina01@gmail.com>
2025-05-20 14:03:54 -0300

    refactor: responsividade notebook

--- script.js
+++ script.js
@@ -26,3 +26,13 @@ window.addEventListener('scroll', () => {
     }
   });
 });
+
+
+//mouse//
+const cursor = document.getElementById('cursor');
+
+  document.addEventListener('mousemove', (e) => {
+    cursor.style.left = `${e.clientX}px`;
+    cursor.style.top = `${e.clientY}px`;
+  });
+
